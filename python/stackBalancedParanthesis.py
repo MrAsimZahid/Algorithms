523b5b7bff0d0c9c35398b2276af2a88b601a0d3
Asim Zahid <asimzahid02@gmaial.com>
2019-11-17 22:06:48 -0500

    Parenthesis balance checker

---
+++ python/stackBalancedParanthesis.py
@@ -0,0 +1,44 @@
+#in this program edge csaes also covered like  "]))}",
+#  "(()", and "([])"
+
+#import stack
+
+from stack import Stack
+
+#parenthesis matching function
+def isMatch(p1, p2):
+    if p1 == "(" and p2 == ")":
+        return True
+    elif p1 == "{" and p2 == "}":
+        return True
+    elif p1 == "[" and "]":
+        return True
+    else:
+        return False
+
+# is parenthesis balanced function
+def isParenBalanced(parenString):
+    s = Stack() # Stack object
+    isBalanced = True
+    index = 0       #current index/ position
+# loop to iterate over for all parenthesis string
+    while index < len(parenString) and isBalanced:
+        paren = parenString[index]
+        if paren in "({[":
+            s.push(paren)
+        else:
+            if s.is_empty():
+                isBalanced = False
+            else:
+                top = s.pop()
+                if not isMatch(top, paren):
+                    isBalanced = False
+        index += 1
+
+    if s.is_empty() and isBalanced:
+        return True
+    else:
+        return False
+
+#Main fuction
+print(isParenBalanced("())"))

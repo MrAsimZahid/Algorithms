23cfc28230510a2b89f1446619a0f605bee1edb4
Asim Zahid <asimzahid02@gmaial.com>
2019-11-17 22:39:01 -0500

    Integer to Binary conversion using stack data structure

---
+++ python/integerToBinary.py
@@ -0,0 +1,19 @@
+from stack import Stack
+
+ef divBy2(decimalNum):
+    s = Stack()
+    
+    while decimalNum > 0:
+        remainder = decimalNum % 2
+        s.push(remainder)
+        decimalNum = decimalNum // 2
+
+
+    binaryNum = ""
+    while not s.is_empty():
+        binaryNum += str(s.pop())
+
+    return binaryNum
+
+
+print(divBy2(242)

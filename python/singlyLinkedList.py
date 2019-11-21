d9e089467a1a2283c2212789c11b7ce5352a6add
Asim Zahid <asimzahid02@gmaial.com>
2019-11-21 15:09:05 -0500

    deletion function/module added

--- python/singlyLinkedList.py
+++ python/singlyLinkedList.py
@@ -6,7 +6,7 @@ class Node:
 class linkedList:
     def __init__(self):
         self.head = None
-
+#Insertion in linked list
 
     def printList(self):
         currentNode = self.head
@@ -35,12 +35,54 @@ class linkedList:
     def insertAfterNode(self, previousNode, data):
         if not previousNode:
             print("previous node is not in the list")
-            return 
+            return
         newNode = Node(data)
         newNode.next = previousNode.next
         previousNode.next = newNode
 
-            
+#Deletion in linked list
+    def deleteNode(self, data):
+        currentNode = self.head
+        if currentNode and currentNode.data == data:
+            self.head = currentNode.next
+            currentNode = None
+            return
+
+        prev = None
+        while currentNode and currentNode.data != data:
+            prev = currentNode
+            currentNode = currentNode.next
+
+        if currentNode is None:
+            return
+
+        prev.next = currentNode.next
+        currentNode = None
+
+#Deletion Node at position
+    def deleteNodePosition(self, pos):
+        
+        currentNode = self.head
+        
+        if pos == 0:
+            self.head = currentNode.next
+            currentNode = None
+            return
+
+        prev = None
+        count = 0
+        while currentNode and count != pos:
+            prev = currentNode
+#s            currentNode = curretNode.next
+            count += 1
+
+        if currentNode is None:
+            print("enter valid address")
+            return
+
+        prev.next = currentNode.next
+        currentNode = None
+
 
 
 llist = linkedList()
@@ -52,7 +94,7 @@ llist.append("d")
 print(llist.head.data)
 llist.prepend("f")
 llist.insertAfterNode(llist.head.next, "E")
-
+#llist.deleteNodePosition(2)
 print(llist.head.data)
-
+llist.deleteNode("f")
 llist.printList()

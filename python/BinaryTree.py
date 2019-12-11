4d8da894e85bc2909e4390edf529320d7db00194
Asim Zahid <asimzahid02@gmaial.com>
2019-12-11 07:49:45 -0500

    Traversals added
    Inorder and postOrder traversal added to the code base

--- python/BinaryTree.py
+++ python/BinaryTree.py
@@ -11,6 +11,10 @@ class BinaryTree(object):
     def print_tree(self, traversal_type):
         if traversal_type == "preorder":
             return self.preorder_print(tree.root, "")
+        elif traversal_type == "inorder":
+            return self.inOrder_print(tree.root, "")
+        elif traversal_type == "postOrder":
+            return self.postOrder_print(tree.root, "")
         else:
             print("Traversal type " + str(traversal_type) + " is not supported")
             return False
@@ -21,6 +25,22 @@ class BinaryTree(object):
             traversal += (str(start.value) + "-")
             traversal = self.preorder_print(start.left, traversal)
             traversal = self.preorder_print(start.right, traversal)
+        return traversal
+
+    def inOrder_print(self, start, traversal):
+	    """ Left -> root -> right"""
+	    if start:
+	        traversal = self.inOrder_print(start.left, traversal)
+	        traversal += (str(start.value) + "-")
+	        traversal =  self.inOrder_print(start.right, traversal)
+	    return traversal
+
+    def postOrder_print(self, start, traversal):
+        """ Left -> right -> root"""
+        if start:
+            traversal = self.inOrder_print(start.left, traversal)
+            traversal =  self.inOrder_print(start.right, traversal)
+            traversal += (str(start.value) + "-")
         return traversal
 
 tree = BinaryTree(1)
@@ -35,3 +55,5 @@ tree.root.right.right.right = Node(8)
 
 
 print(tree.print_tree("preorder"))
+print(tree.print_tree("postOrder"))
+print(tree.print_tree("inorder"))

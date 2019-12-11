c9cd899595c789d5857aafc8bde875efadc9e7c7
Asim Zahid <asimzahid02@gmaial.com>
2019-12-11 16:19:43 -0500

    Level order traversal
    Level order traversal added into Binarytree.py along with queue class and rewrite the length(len) function to find the sife of items.

--- python/BinaryTree.py
+++ python/BinaryTree.py
@@ -1,3 +1,32 @@
+#Class Queue
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+#Enter value in queue
+    def enqueue(self, item):
+        return self.items.insert(0, item)
+#Pop values from queue
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+    
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+ #Redefine length function   
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+#-----------------------------------------------
+
+
+#Binary Tree
 #Node class/structure
 class Node(object):
     def __init__(self, value):
@@ -18,6 +47,10 @@ class BinaryTree(object):
             return self.inOrder_print(tree.root, "")
         elif traversal_type == "postOrder":
             return self.postOrder_print(tree.root, "")
+        elif traversal_type == "levelorder":
+            return self.levelOrder_print(tree.root)
+        elif traversal_type == "levelorder":
+            return self.levelOrder_print(tree.root)
         else:
             print("Traversal type " + str(traversal_type) + " is not supported")
             return False
@@ -48,7 +81,24 @@ class BinaryTree(object):
             traversal =  self.inOrder_print(start.right, traversal)
             traversal += (str(start.value) + "-")
         return traversal
+#Method for level order traversal
+    def levelOrder_print(self, start):
+        if start is None:
+            return
 
+        queue = Queue()
+        queue.enqueue(start)
+
+        traversal = ""
+        while len(queue) > 0:
+            traversal += str(queue.peek()) + "-"
+            node = queue.dequeue()
+
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+        return traversal
 
 #Main function
 #Enter values in the tree
@@ -66,3 +116,4 @@ tree.root.right.right.right = Node(8)
 print(tree.print_tree("preorder"))
 print(tree.print_tree("postOrder"))
 print(tree.print_tree("inorder"))
+print(tree.print_tree("levelorder"))

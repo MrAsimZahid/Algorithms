5739de227678a561002e52bb2935b6b7ff2fdca3
Asim Zahid <asimzahid02@gmaial.com>
2019-12-11 22:56:48 -0500

    Basic Binary Search Tree

---
+++ python/BinarySearchTree.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, data = None):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+    
